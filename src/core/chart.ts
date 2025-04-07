f929107da44dde92740522af883074a5ee5a56ef
Уэйд Уилсон <usasupsn@gmail.com>
2025-04-07 20:57:23 +0200

    chore:   bun.lock 	chore:   package.json 	fix:   src/core/chart.ts

--- src/core/chart.ts
+++ src/core/chart.ts
@@ -1,24 +1,13 @@
-import axios from 'axios';
 import { Canvas } from 'canvas';
 import { Chart } from 'chart.js/auto';
 
 import type { ChartResponse } from '@/types/chart';
 
 export async function generateChart(pool: string, token: string) {
-	const response = await axios.get<ChartResponse>(
-		`https://datapi.jup.ag/v1/charts/${pool}`,
-		{
-			params: {
-				interval: '15_SECOND',
-				baseAsset: token,
-				from: Date.now() - 60 * 60 * 1000,
-				to: Date.now(),
-				candles: 300,
-				type: 'price',
-			},
-		},
+	const response = await fetch(
+		`https://datapi.jup.ag/v1/charts/${pool}?interval=15_SECOND&baseAsset=${token}&from=${Date.now() - 60 * 60 * 1000}&to=${Date.now()}&candles=300&type=price`,
 	);
-	const data = response.data.candles;
+	const data: ChartResponse = await response.json();
 
 	const canvas = new Canvas(2000, 1500);
 	const canvasElement = {
@@ -29,25 +18,25 @@ export async function generateChart(pool: string, token: string) {
 	} as unknown as HTMLCanvasElement;
 	const ctx = canvasElement.getContext('2d');
 
-	const maxVolume = Math.max(...data.map(item => item.volume));
+	const maxVolume = Math.max(...data.candles.map(item => item.volume));
 	const targetMaxVolumeHeight = canvas.height / 5;
 
 	const chart = new Chart(canvasElement, {
 		type: 'line',
 		data: {
-			labels: data.map(item =>
+			labels: data.candles.map(item =>
 				new Date(item.time * 1000).toLocaleTimeString('en-GB'),
 			),
 			datasets: [
 				{
-					data: data.map(item => item.close),
+					data: data.candles.map(item => item.close),
 					borderColor: 'white',
 					borderWidth: 2,
 					fill: false,
 					yAxisID: 'y',
 				},
 				{
-					data: data.map(item => item.volume),
+					data: data.candles.map(item => item.volume),
 					backgroundColor: 'rgba(0, 123, 255, 0.5)',
 					borderColor: 'blue',
 					borderWidth: 1,
